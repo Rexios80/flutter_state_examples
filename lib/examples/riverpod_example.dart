7073b2dd15e19b827930119941123c1658a2da81
Rexios <rexios@rexios.dev>
2023-10-26 11:41:41 -0400

    Add riverpod example

--- lib/examples/riverpod_example.dart
+++ lib/examples/riverpod_example.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_state_examples/examples/counter_widget.dart';
 
-class RiverpodExample extends StatelessWidget with CounterWidget {
+class RiverpodExample extends ConsumerStatefulWidget {
   const RiverpodExample({super.key});
 
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _RiverpodExampleState();
+}
+
+final counterProvider = StateProvider((ref) => 0);
+
+class _RiverpodExampleState extends ConsumerState<RiverpodExample>
+    with CounterWidget {
   @override
   final title = 'Riverpod';
 
   @override
   void increment() {
-    // TODO: implement increment
+    ref.read(counterProvider.notifier).state++;
   }
 
   @override
-  // TODO: implement builder
-  Widget get builder => throw UnimplementedError();
+  Widget get builder {
+    return Consumer(
+      builder: (context, ref, _) {
+        final count = ref.watch(counterProvider);
+        return Text('$count');
+      },
+    );
+  }
 }
+
+// This actually cannot be written as a ConsumerWidget since the build method
+// is different
+
+// build(BuildContext context, WidgetRef ref) {}
